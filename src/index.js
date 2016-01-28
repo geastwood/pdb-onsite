7eb2b0cccb2e9c56ac310d5bb9080d87b8d9071f
Fei Liu <liu.feiwood@gmail.com>
2016-01-28 23:51:34 +0100

    working in progress

--- src/index.js
+++ src/index.js
@@ -1,33 +1,4 @@
-import lorem from '../data/lorem';
-import testData from '../data/testData.js';
 import fetch from 'isomorphic-fetch';
-
-var randomKeywords = (length, data) => {
-  var keys = Object.keys(testData),
-    total = keys.length,
-    rst = [];
-  do {
-    rst.push(keys[Math.floor(Math.random() * total)]);
-    length--;
-  } while (length > 0);
-  return rst;
-};
-
-var buildParagraph = (lorems, keywords) => {
-  return keywords.reduce((carry, keyword) => {
-    var random = Math.floor(Math.random() * carry.length);
-    return carry.slice(0, random).concat(keyword).concat(carry.slice(random));
-  }, lorems).join(' ');
-};
-
-var keywords = randomKeywords(3, testData);
-
-//var preprareMarkUp = keywords => {
-//  return keywords.map(key => {
-//    var detail = testData[key];
-//    return
-//  })
-//};
 
 var connect = (src, fn) => {
   var script = document.createElement('script');
@@ -51,6 +22,8 @@ export var request = key => {
         connect('http://localhost:3000/javascripts/initializer.js', function() {
           PdbConnect.init();
         });
+      } else {
+        PdbConnect.init();
       }
     });
 };
